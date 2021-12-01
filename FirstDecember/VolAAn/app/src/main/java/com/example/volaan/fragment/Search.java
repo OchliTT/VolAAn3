adef1c61d0d49f790c65ad270cd2d37d9866851d
Ochli_T_T_ <ochlinetime@gmail.com>
2021-12-01 21:38:50 +0300

    VersionOfFirstDecember

---
+++ FirstDecember/VolAAn/app/src/main/java/com/example/volaan/fragment/Search.java
@@ -0,0 +1,78 @@
+package com.example.volaan.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.example.volaan.Main;
+import com.example.volaan.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Search#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Search extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    LinearLayout l;
+    ImageButton b;
+    EditText edit;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Search() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Search.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Search newInstance(String param1, String param2) {
+        Search fragment = new Search();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_search, null);
+        edit= v.findViewById(R.id.EditSearch);
+        b= v.findViewById(R.id.SearchButton);
+        l= v.findViewById(R.id.LayoutSearch);
+
+        Main ma = (Main) getActivity();
+        ma.user_search(edit,b,l);
+        return v;
+    }
+}
